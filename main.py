e8a5c1870787ded3d51d92d3a7551c0a006649b4
sgarabiles-10r <garabiles.sg@obmontessori.edu.ph>
2026-02-27 07:13:08 +0800

    Refactor login and team checker functions

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from pyscript import display, document
 
-# ----------- LOGIN FIRST STEP (GATE THAT SHOWS UP FIRST)-----------
+# ----------- LOGIN (GATE THAT SHOWS UP FIRST)-----------
 accounts = {}
 
 def checking_account(e):
@@ -27,13 +27,13 @@ def checking_account(e):
 
                 display("Congratulations! you now have an account", target="account-output")
 
-                # Hide login section
+                # hide login section
                 document.getElementById("account-section").style.display = "none"
                 
                 # Show tabs section
                 document.getElementById("tabs-section").style.display = "block"
 
-                # Add logged-in class to body to activate full GIF/background
+                # the intiator for the 2nd css/logged in
                 document.querySelector("body").classList.add("logged-in")
 
             else:
@@ -44,12 +44,13 @@ def checking_account(e):
         display("Username must be at least 7 characters long", target="account-output")
 
 
-# ----------- TEAM CHECKER -----------
+# ----------- TEAM CHECKER / 1st tab -----------
 def check_team(e):
     if "username" not in accounts:
         display("Please create an account first!", target="output")
         return
 
+    # getting the value of like the input field, and then confirming it a variables
     registered = document.querySelector('input[name="registration"]:checked')
     medical = document.querySelector('input[name="clear"]:checked')
     grade = document.getElementById("level").value
@@ -57,6 +58,8 @@ def check_team(e):
     document.getElementById("output").innerHTML = ""
     document.getElementById("image").innerHTML = ""
 
+
+    # is none" is when the input is no value/null
     if registered is None or medical is None:
         display("Please select registration and medical clearance options.", target="output")
         return
@@ -91,7 +94,7 @@ def check_team(e):
         document.getElementById("image").innerHTML = "<img src='sampel.image' height='300' width='200'>"
 
 
-# ----------- PLAYERS LIST -----------
+# ----------- players (dwayne) -----------
 def players_list(e):
     if "username" not in accounts:
         display("Please create an account first!", target="result")
@@ -102,6 +105,7 @@ def players_list(e):
              "Sangreo", "Ala", "Baring", "Olmedo", "Broadhagen", "Rebadulla", "Reyes", 
              "Jamet", "Gonzalez", "Ledesma", "Agena", "Nacino", "Tarachand", "Cervantes"]
 
-    document.getElementById("result").innerHTML = ""  # clear previous
+    document.getElementById("result").innerHTML = ""  # so like theres no duplicates
     for i, name in enumerate(names, start=1):
+
         display(f"{i}. {name}", target="result")
